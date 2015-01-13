abded47239df9459daa6d219f79b88acb1e1508a
Prasanna Pandey <ppandey@deerwalk.com>
2015-01-13 17:22:22 +0545

    Salary report wip...

--- grails-app/domain/com/bayalpatra/hrm/SalaryReport.groovy
+++ grails-app/domain/com/bayalpatra/hrm/SalaryReport.groovy
@@ -4,14 +4,16 @@ class SalaryReport {
 
     Employee employee
     Date salaryDate
+    Date salaryMonth
     Double tax
     Double total
 
 
     static constraints = {
-        employee(blank:false, unique:'salaryDate')
+        employee(blank:false, unique:'salaryMonth')
         tax(scale:2)
         salaryDate(blank:false)
+        salaryMonth(blank:false)
         total(blank:false,scale:2)
     }
 }
